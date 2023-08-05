1616a7298fb8b9c27443f43d0fbd7aa297f377d8
Shishmakova Daria <dary_shishka@mail.ru>
2023-08-05 19:08:08 +0300

    added models

---
+++ FakeNFT/Models/UserModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserModel.swift
+//  FakeNFT
+//
+//  Created by Дарья Шишмакова on 05.08.2023.
+//
+
+import Foundation
+
+struct UserModel {
+    let name: String
+    let avatar: String
+    let description: String
+    let website: String
+    let nfts: [String]
+    let rating: String
+    let id: String
+}
